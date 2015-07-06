8f1350b52a03904373673e7fa60b1f132052d20a
Greg Blumberg <wblumberg@ou.edu>
2015-07-06 00:34:34 -0500

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyLBLRTM',
+      version='1.0',
+      description='Python scripts to run and analyze the LBLRTM output',
+      author='Greg Blumberg',
+      author_email='wblumberg@ou.edu',
+)
+
+
